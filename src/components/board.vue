39229e011f6c348b4a52b8a9d15c6100119f4657
Kathryn Rasmussen <kathryn.rasmussen12@gmail.com>
2018-03-14 17:36:52 -0600

    its a start

---
+++ src/components/board.vue
@@ -0,0 +1,33 @@
+<template>
+  <div >
+    <h2>BOARD </h2>
+    <h2>BOARD </h2>
+    <h2>BOARD </h2>
+    <h2>BOARD </h2>
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Board"
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
