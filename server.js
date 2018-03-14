88ab5645bb73f2233674ebacde01b037d25c1811
Kathryn Rasmussen <kathryn.rasmussen12@gmail.com>
2018-03-13 20:34:08 -0600

    API done

--- server.js
+++ server.js
@@ -1,26 +1,53 @@
-// COOL DUDE
-
 const express = require('express');
+const bodyParser = require('body-parser');
+
 const app = express();
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use(express.static('public'))
+
+
+let ideas = [];
+let id = 0;
 
 //Used to get all ideas
 app.get('/idea', (req, res) => {
-  res.send('Get Idea was hit!');
+    res.send(ideas);
 });
 
 //used to add a single idea
-app.post('/idea/:id', (req, res) => {
-    res.send('POST Idea was hit!');
-  });
+app.post('/idea', (req, res) => {
+    id = id + 1;
+    let idea = { id: id, text: req.body.text, likes: req.body.likes, dislikes: req.body.dislikes };
+    ideas.push(idea);
+    res.send(idea);
+});
 
-  //used to update a single idea
+//used to update a single idea
 app.put('/idea/:id', (req, res) => {
-    res.send('PUT Idea was hit!');
-  });
+    let id = parseInt(req.params.id);
+    let ideasMap = ideas.map(idea => { return idea.id; });
+    let index = ideasMap.indexOf(id);
+    let idea = ideas[index];
 
-  //used to add a single idea
+    idea.likes = req.body.likes;
+    idea.dislikes = req.body.dislikes;
+
+    res.send(idea);
+});
+
+//used to add a single idea
 app.delete('/idea/:id', (req, res) => {
-    res.send('DELETE Idea was hit!');
-  });
+    let id = parseInt(req.params.id);
+    let removeIndex = ideas.map(idea => { return idea.id; }).indexOf(id);
+    if (removeIndex === -1) {
+        res.status(404).send("Houston we have a problem");
+        return;
+    }
+
+    ideas.splice(removeIndex, 1);
+    res.sendStatus(200);
+});
 
 app.listen(3000, () => console.log('Server listening on port 3000!'));
