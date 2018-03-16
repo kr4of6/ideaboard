5539c7f169daf1c458796059e6142add22a69e49
Kathryn Rasmussen <kathryn.rasmussen12@gmail.com>
2018-03-15 20:27:16 -0600

    cors

--- server.js
+++ server.js
@@ -1,11 +1,14 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+var cors = require('cors');
+
 
 const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
 app.use(express.static('public'))
+app.use(cors());
 
 
 let ideas = [];
